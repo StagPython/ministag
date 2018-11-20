931466e90ed59d6c7cd50a7438862d8f54d3c9cb
Adrien Morison <adrien.morison@gmail.com>
2018-11-20 18:08:55 +0100

    Implement stokes solver

--- ministag/solver.py
+++ ministag/solver.py
@@ -1,4 +1,6 @@
 import pathlib
+from scipy.sparse.linalg import factorized
+import scipy.sparse as sp
 import matplotlib.pyplot as plt
 import numpy as np
 import toml
@@ -21,6 +23,7 @@ class RayleighBenardStokes:
         self.v_x = None
         self.v_z = None
         self.dynp = None
+        self.eta = None
         self._restart = pars.get('restart', {}).get('file', None)
 
     def _outfile_stem(self, name, istep):
@@ -51,8 +54,137 @@ class RayleighBenardStokes:
         plt.close()
 
     def _stokes(self):
-        self.v_x = None
-        self.v_z = None
+        d_z = 1 / self.n_z
+        if self.var_visc:
+            a_visc = np.log(self.var_visc_temp)
+            b_visc = np.log(self.var_visc_depth)
+            depth = 0.5 - np.linspace(0.5 * d_z, 1 - 0.5 * d_z, self.n_z)
+            self.eta = np.exp(-a_visc * (self.temp - 0.5) + b_visc * depth)
+        else:
+            self.eta = np.ones((self.n_x, self.n_z))
+        rhsz = np.zeros((self.n_x, self.n_z))
+        rhsz[:, 1:] = -self.ranum * (self.temp[:, :-1] + self.temp[:, 1:]) / 2
+
+        odz = self.n_z
+        odz2 = odz**2
+
+        rhs = np.zeros(self.n_x * self.n_z * 3)
+        # indices offset
+        idx = 3
+        idz = self.n_x * 3
+        n_non0 = (11 + 11 + 4) * self.n_x * self.n_z
+        rows = np.zeros(n_non0)
+        cols = np.zeros(n_non0)
+        coefs = np.zeros(n_non0)
+        n_non0 = 0  # track number of non zeros coef
+
+        def mcoef(row, col, coef):
+            nonlocal n_non0
+            rows[n_non0] = row
+            cols[n_non0] = col
+            coefs[n_non0] = coef
+            n_non0 += 1
+
+        for iz in range(self.n_z):
+            for ix in range(self.n_x):
+                icell = ix + iz * self.n_x
+                ieqx = icell * 3
+                ieqz = ieqx + 1
+                ieqc = ieqx + 2
+
+                etaii_c  = self.eta[ix, iz]
+                if ix > 0:
+                    etaii_xm = self.eta[ix-1, iz]
+                if iz > 0:
+                    etaii_zm = self.eta[ix, iz-1]
+                if ix > 0 and iz > 0:
+                    etaxz_c = (self.eta[ix, iz] * self.eta[ix - 1, iz] *
+                               self.eta[ix, iz - 1] *
+                               self.eta[ix - 1, iz - 1])**0.25
+                else:
+                    etaxz_c = 0
+                if ix > 0 and iz < self.n_z - 1:
+                    etaxz_zp = (self.eta[ix, iz + 1] *
+                                self.eta[ix - 1, iz + 1] * self.eta[ix, iz] *
+                                self.eta[ix - 1, iz])**0.25
+                else:
+                    etaxz_zp = 0
+                if ix < self.n_x-1 and iz > 0:
+                    etaxz_xp = (self.eta[ix + 1, iz] * self.eta[ix, iz] *
+                                self.eta[ix + 1, iz - 1] *
+                                self.eta[ix, iz - 1])**0.25
+                else:
+                    etaxz_xp = 0
+
+                xmom_zero_eta = (etaii_c == 0 and etaii_xm == 0 and
+                                 etaxz_c == 0 and etaxz_zp == 0)
+                zmom_zero_eta = (etaii_c == 0 and etaii_zm == 0 and
+                                 etazx_c == 0 and etazx_xp == 0)
+
+                # x-momentum
+                if ix > 0 and not xmom_zero_eta:
+                    mcoef(ieqx, ieqx, -odz2 * (2 * etaii_c + 2 * etaii_xm +
+                                               etaxz_c + etaxz_zp))
+                    mcoef(ieqx, ieqx - idx, 2 * odz2 * etaii_xm)
+                    mcoef(ieqx, ieqz, -odz2 * etaxz_c)
+                    mcoef(ieqx, ieqz - idx, odz2 * etaxz_c)
+                    mcoef(ieqx, ieqc, -odz)
+                    mcoef(ieqx, ieqc - idx, odz)
+
+                    if ix + 1 < self.n_x:
+                        mcoef(ieqx, ieqx + idx, 2 * odz2 * etaii_c)
+                    if iz + 1 < self.n_z:
+                        mcoef(ieqx, ieqx + idz, odz2 * etaxz_zp)
+                        mcoef(ieqx, ieqz + idz, odz2 * etaxz_zp)
+                        mcoef(ieqx, ieqz + idz - idx, -odz2 * etaxz_zp)
+                    if iz > 0:
+                        mcoef(ieqx, ieqx - idz, odz2 * etaxz_c)
+                    rhs[ieqx] = 0
+                else:
+                    mcoef(ieqx, ieqx, 1)
+                    rhs[ieqx] = 0
+
+                # z-momentum
+                if iz > 0 and not zmom_zero_eta:
+                    mcoef(ieqz, ieqz, -2 * odz2 * (2 * etaii_c + 2 * etaii_zm +
+                                                   etaxz_c + etaxz_xp))
+                    mcoef(ieqz, ieqz - idz, 2 * odz2 * etaii_zm)
+                    mcoef(ieqz, ieqx, -odz2 * etaxz_c)
+                    mcoef(ieqz, ieqx - idz, odz2 * etaxz_c)
+                    mcoef(ieqz, ieqc, -odz)
+                    mcoef(ieqz, ieqc - ieqz, odz)
+
+                    if iz + 1 < self.n_z:
+                        mcoef(ieqz, ieqz + idz, 2 * odz2 * etaii_c)
+                    if ix + 1 < self.n_x:
+                        mcoef(ieqz, ieqz + idx, odz2 * etaxz_xp)
+                        mcoef(ieqz, ieqx + idx, odz2 * etaxz_xp)
+                        mcoef(ieqz, ieqx + idx - idz, -odz2 * etaxz_xp)
+                    if ix > 0:
+                        mcoef(ieqz, ieqz - idx, odz2 * etaxz_c)
+                    rhs[ieqz] = rhsz[ix, iz]
+                else:
+                    mcoef(ieqz, ieqz, 1)
+                    rhs[ieqz] = 0
+
+                # continuity
+                if (ix==0 and iz==0) or (xmom_zero_eta and zmom_zero_eta):
+                    mcoef(ieqc, ieqc, 1)
+                else:
+                    mcoef(ieqc, ieqx, -odz)
+                    mcoef(ieqc, ieqz, -odz)
+                    if ix + 1 < self.n_x:
+                        mcoef(ieqc, ieqx + idx, odz)
+                    if iz + 1 < self.n_z:
+                        mcoef(ieqc, ieqz + idz, odz)
+                rhs[ieqc] = 0
+
+        fsolve = factorized(sp.coo_matrix((coefs, (rows, cols)),
+                                          shape=(rhs.size, rhs.size)))
+        sol = fsolve(rhs)
+        self.v_x = np.reshape(sol[::3], (self.n_x, self.n_z))
+        self.v_z = np.reshape(sol[1::3], (self.n_x, self.n_z))
+        self.dynp = np.reshape(sol[2::3], (self.n_x, self.n_z))
 
     def _heat(self):
         pass#self.temp = None
@@ -73,6 +205,7 @@ class RayleighBenardStokes:
         for istep in range(restart + 1, self.nsteps + 1):
             self._stokes()
             self._heat()
+            # time series
             if istep % self.nwrite == 0:
                 self._save(istep)
 
