1d0de16c232ec33864d12a7d033f37bb57f76296
Stephane Labrosse <stephane@makatea.lst.ens-lyon.fr>
2018-11-26 16:35:08 +0100

    added streamlines on the temperature plot

--- ministag/solver.py
+++ ministag/solver.py
@@ -46,22 +46,6 @@ class RayleighBenardStokes:
                 self.temp = self.temp_init + \
                     0.01 * np.random.rand(self.n_x, self.n_z)
 
-    def _plot(self, istep, xgrid, zgrid, field, label, fname=None):
-        if fname is None:
-            fname = label
-        fig, axis = plt.subplots()
-        surf = axis.pcolormesh(xgrid, zgrid, field.T, cmap='RdBu_r',
-                               shading='gouraud')
-        cbar = plt.colorbar(surf, shrink=0.5)
-        cbar.set_label(label)
-        plt.axis('equal')
-        plt.axis('off')
-        axis.set_adjustable('box')
-        axis.set_xlim(0, self.n_x / self.n_z)
-        axis.set_ylim(0, 1)
-        fig.savefig(self._outfile_stem(fname, istep) + '.pdf',
-                    bbox_inches='tight')
-        plt.close(fig)
 
     def _save(self, istep):
         pathlib.Path('output').mkdir(exist_ok=True)
@@ -70,9 +54,31 @@ class RayleighBenardStokes:
 
         xgrid = np.linspace(0, self.n_x / self.n_z, self.n_x)
         zgrid = np.linspace(0, 1, self.n_z)
-        self._plot(istep, xgrid, zgrid, self.temp, 'Temperature', 'T')
-        self._plot(istep, xgrid, zgrid, self.v_x, 'vx')
-        self._plot(istep, xgrid, zgrid, self.v_z, 'vz')
+        fig, axis = plt.subplots()
+        # first plot the temperature field
+        surf = axis.pcolormesh(xgrid, zgrid, self.temp.T, cmap='RdBu_r',
+                               shading='gouraud')
+        cbar = plt.colorbar(surf, shrink=0.5)
+        cbar.set_label('Temperature')
+        plt.axis('equal')
+        plt.axis('off')
+        axis.set_adjustable('box')
+        axis.set_xlim(0, self.n_x / self.n_z)
+        axis.set_ylim(0, 1)
+        # interpolate velocities at the same points for streamlines
+        u_x = 0.5 * (self.v_x + np.roll(self.v_x, 1, 0))
+        u_z = np.zeros_like(self.v_z)
+        u_z[1:] = self.v_z[:-1]
+        u_z += self.v_z
+        u_z *= 0.5
+        speed = np.sqrt(u_x * u_x + u_z * u_z)
+        # plot the streamlines
+        lw = 2*speed / speed.max()
+        axis.streamplot(xgrid, zgrid, u_x.T, u_z.T, color='k',
+                            linewidth=lw.T)
+        fig.savefig(self._outfile_stem('T_v', istep) + '.pdf',
+                    bbox_inches='tight')
+        plt.close(fig)
 
     def _stokes(self):
         if self.var_visc:
